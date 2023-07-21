f2fe8e47890d0c2bb2e4f997a848e6df43405110
Alex Bass <alexpjbass@gmail.com>
2023-07-21 19:25:35 +0200

    Add routing and artist page

--- components/ArtistLayout.tsx
+++ components/ArtistLayout.tsx
@@ -7,13 +7,16 @@ import ArtistProfile from "./ArtistProfile";
 
 export interface ArtistListProps {
   artists: Artist[];
+  selectedArtist?: Artist;
 }
 
 export default function ArtistLayout(props: ArtistListProps) {
+  const { artists, selectedArtist } = props;
+
   const [selected, setHovered] = useState<Artist | null>(null);
 
   const setHoveredItem = (key: Artist["_id"]) => {
-    const artist = props.artists.find((artist) => artist._id === key);
+    const artist = artists.find((artist) => artist._id === key);
     if (artist) setHovered(artist);
   };
 
@@ -21,16 +24,22 @@ export default function ArtistLayout(props: ArtistListProps) {
     setHovered(null);
   };
 
+  const isHoveringSelectedArtist =
+    selected && selectedArtist?._id === selected._id;
+
   return (
     <>
       <ArtistList
-        content={props.artists}
+        content={artists}
         onHover={(name) => setHoveredItem(name)}
         onMouseLeave={setDefaultContent}
+        defaultSelected={selectedArtist}
       />
       <main className="h-full">
-        {selected ? (
+        {selected && !isHoveringSelectedArtist ? (
           <ArtistProfile artist={selected} />
+        ) : selectedArtist ? (
+          <ArtistProfile artist={selectedArtist} full />
         ) : (
           <section
             className="h-full bg-cover bg-center flex flex-col"
--- components/ArtistList.tsx
+++ components/ArtistList.tsx
@@ -1,32 +1,40 @@
 "use client";
 
 import Link from "next/link";
-import { Artist } from "../types/Artist";
+import clsx from "clsx";
+import { Artist } from "@/types/Artist";
 
 export interface ArtistListProps {
+  defaultSelected?: Artist;
   onMouseLeave: () => void;
   onHover: (key: string) => void;
   content: Artist[];
 }
 
 export default function ArtistList(props: ArtistListProps) {
+  const { content, defaultSelected, onMouseLeave, onHover } = props;
+
   return (
-    <nav className="nav h-full" onMouseLeave={props.onMouseLeave}>
+    <nav className="nav h-full" onMouseLeave={onMouseLeave}>
       <ul className="grid h-full auto-rows-auto">
-        {props.content.map((artist) => {
-          const id = artist._id;
+        {content.map((artist) => {
+          const { _id: id } = artist;
           return (
             <li
               key={id}
               tabIndex={0}
               // className="flex px-2 border-t border-violet-500 "
-              className="font-extrabold text-2xl text-black p-2 hover:bg-gradient-to-r from-violet-400 to-violet-600 transition hover:text-white transition"
-              onMouseEnter={() => props.onHover(id)}
-              onFocus={() => props.onHover(id)}
+              className={clsx(
+                "font-extrabold text-2xl text-black hover:bg-gradient-to-r from-violet-400 to-violet-600 transition hover:text-white transition",
+                defaultSelected?._id === id &&
+                  "bg-gradient-to-r from-violet-400 to-violet-600 text-white"
+              )}
+              onMouseEnter={() => onHover(id)}
+              onFocus={() => onHover(id)}
             >
               <Link
                 href={`/artists/${artist.slug}`}
-                className="w-full h-full flex items-center"
+                className="w-full h-full flex items-center p-2"
               >
                 {artist.name}
               </Link>

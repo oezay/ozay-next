b84e99fd56d1164f437b2d0866c299f80ee550c3
Alex Bass <alexpjbass@gmail.com>
2023-09-17 11:15:29 +0200

    Set useCdn to true

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import schemas from "./sanity/schemas";
-
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import schemas from './sanity/schemas'
 
 const config = defineConfig({
-    projectId: "tpdsqkwn",
-    dataset: "production",
-    title: "My Personal Website",
-    apiVersion: "2023-04-11",
-    basePath: "/admin",
-    plugins: [deskTool()], 
-    schema: { types: schemas }
-});
+  projectId: 'tpdsqkwn',
+  dataset: 'production',
+  title: 'My Personal Website',
+  apiVersion: '2023-04-11',
+  basePath: '/admin',
+  plugins: [deskTool()],
+  schema: { types: schemas },
+})
 
-export default config;
+export default config
